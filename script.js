f5dda92a862288371f6874e758cf6d38007f1d1b
smarko-web <smarko4web@gmail.com>
2023-09-05 16:43:29 -0500

    add preachers

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const copyButton = document.querySelector('.copy-post');
 
 var url, message; 
 const singerList = ['Robin Herd', 'Randy Knaps', 'Joseph Larson', 'Grace Larson Brumley', 'Jill Swaggart', 'Tara Montpetit', 'Kim Coleman', 'BJ Pons','Brian Haney', 'FWC choir and singers', 'FWC singers'];
-const preacherList = ['Jimmy Swaggart', 'Donnie Swaggart', 'Gabriel Swaggart', 'Loren Larson', 'Mike Muzzerall', 'David Smith', 'Dale Usey', 'Dr. Don Paul Gray', 'Paris Ragan', 'Shaun Murphy', 'Jonathan Steele', 'Dr. Dave Watts',
+const preacherList = ['Jimmy Swaggart', 'Donnie Swaggart', 'Gabriel Swaggart', 'Loren Larson', 'Mike Muzzerall', 'David Smith', 'John Rosenstern', 'Josh Rosenstern', 'Dale Usey', 'Dr. Don Paul Gray', 'Paris Ragan', 'Shaun Murphy', 'Jonathan Steele', 'Dr. Dave Watts',
 'Dr. Jimmy DuPree'];
 
 for(var i = 0; i < singerList.length - 2; i++) {
